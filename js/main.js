4f634a46f2dd65a03ae9b28dac3c6f3965e6c0bd
AlexeyRezanov <Alexey_Rezanov22@mail.ru>
2024-09-28 19:26:36 +0500

    удаляет импорт функции createPhotosArray из файла main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-import {createPhotosArray} from './data.js';
 import './upload-img.js';
 import './redact-img.js';
 import './send-data.js';
 import './view-img.js';
 import './filter-img.js';
 import './thumbnails.js';
-
-// console.log(createPhotosArray());
