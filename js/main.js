917e26a3ea7105fc9a86910eec825033d129495f
AlexeyRezanov <Alexey_Rezanov22@mail.ru>
2024-09-28 18:45:52 +0500

    Отображает миниатюры фотографий, выводит количество лайков и комментариев.

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ import './redact-img.js';
 import './send-data.js';
 import './view-img.js';
 import './filter-img.js';
+import './thumbnails.js';
 
-console.log(createPhotosArray());
+// console.log(createPhotosArray());
