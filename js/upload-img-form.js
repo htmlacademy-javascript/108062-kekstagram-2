c77f57123df5c9651ef6a1b174cf76cd66725782
AlexeyRezanov <Alexey_Rezanov22@mail.ru>
2024-11-16 12:19:40 +0500

    Добавляет валидацию, проверки введённых данных

---
+++ js/upload-img-form.js
@@ -0,0 +1,141 @@
+import {isEscapeKey} from './utils.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const uploadFileInput = uploadForm.querySelector('#upload-file');
+const imgEditorForm = uploadForm.querySelector('.img-upload__overlay');
+const imgEditorResetBtn = imgEditorForm.querySelector('#upload-cancel');
+const hashtagInput = uploadForm.querySelector('.text__hashtags');
+const commentInput = uploadForm.querySelector('.text__description');
+
+const MAX_COMMENT_SYMBOLS = 140;
+const MAX_HASHTAG_SYMBOLS = 20;
+const MAX_HASHTAGS = 5;
+
+let errorMessage = '';
+
+const error = () => errorMessage;
+
+const onImgEditorResetBtnClick = (evt) => {
+  evt.preventDefault();
+  uploadForm.reset();
+  closeImgEditor();
+};
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    if (document.activeElement === hashtagInput || document.activeElement === commentInput) {
+      evt.stopPropagation();
+    } else {
+      uploadForm.reset();
+      closeImgEditor();
+    }
+  }
+};
+
+function closeImgEditor () {
+  imgEditorForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  imgEditorResetBtn.removeEventListener('click', onImgEditorResetBtnClick);
+  uploadFileInput.value = '';
+}
+
+const openUploadModal = () => {
+  uploadFileInput.addEventListener('change', () => {
+    imgEditorForm.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+    document.addEventListener('keydown', onDocumentKeydown);
+    imgEditorResetBtn.addEventListener('click', onImgEditorResetBtnClick);
+  });
+};
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__form',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
+
+const onUploadFormSubmit = (evt) => {
+  evt.preventDefault();
+
+  if (pristine.validate()) {
+    hashtagInput.value = hashtagInput.value.trim().replaceAll(/\s+/g, ' ');
+    uploadForm.submit();
+  }
+};
+
+const isHashtagsValid = (value) => {
+  errorMessage = '';
+
+  const inputText = value.toLowerCase().trim();
+
+  if (!inputText) {
+    return true;
+  }
+
+  const inputArray = inputText.split(/\s+/);
+
+  const rules = [
+    {
+      check: inputArray.some((item) => item === '#'),
+      error: 'Хештег не может состоять только из одной решётки',
+    },
+    {
+      check: inputArray.some((item) => item.slice(1).includes('#')),
+      error: 'Хэштеги разделяются пробелами',
+    },
+    {
+      check: inputArray.some((item) => item[0] !== '#'),
+      error: 'Хэштег должен начинеться с символа \'#\'',
+    },
+    {
+      check: inputArray.some((item, num, array) => array.includes(item, num + 1)),
+      error: 'Один и тот же хэштег не может быть использован дважды',
+    },
+    {
+      check: inputArray.some((item) => item.length > MAX_HASHTAG_SYMBOLS),
+      error: `Максимальная длина одного хэштега ${MAX_HASHTAG_SYMBOLS} символов, включая решётку`,
+    },
+    {
+      check: inputArray.length > MAX_HASHTAGS,
+      error: `нельзя указать больше ${MAX_HASHTAGS} хэштегов`,
+    },
+    {
+      check: inputArray.some((item) => !/^#[a-zа-яё0-9]{1,19}$/i.test(item)),
+      error: 'Хэштег содержит недопустимые символы',
+    },
+  ];
+
+  return rules.every((rule) => {
+    const isInvalid = rule.check;
+    if (isInvalid) {
+      errorMessage = rule.error;
+    }
+    return !isInvalid;
+  });
+};
+
+const onHashtagInput = () => {
+  isHashtagsValid(hashtagInput.value);
+};
+
+pristine.addValidator(
+  hashtagInput,
+  isHashtagsValid,
+  error
+);
+
+pristine.addValidator (
+  commentInput,
+  (value) => {
+    const hasNumber = value.length <= MAX_COMMENT_SYMBOLS;
+    return hasNumber;
+  },
+  `длина комментария не может составлять больше ${MAX_COMMENT_SYMBOLS} символов`
+);
+
+hashtagInput.addEventListener('input', onHashtagInput);
+uploadForm.addEventListener('submit', onUploadFormSubmit);
+
+export {openUploadModal};
