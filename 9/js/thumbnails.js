21400bb49a5069a64dbb475712ba07df4e8a313a
Keks <keks@htmlacademy.ru>
2024-11-21 15:26:42 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/thumbnails.js
@@ -0,0 +1,25 @@
+import {photos} from './data.js';
+
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const containerPictures = document.querySelector('.pictures');
+const picturesFragment = document.createDocumentFragment();
+const image = templatePicture.querySelector('.picture__img');
+const pictureComments = templatePicture.querySelector('.picture__comments');
+const pictureLikes = templatePicture.querySelector('.picture__likes');
+
+photos.forEach(({id, url, description, comments, likes}) => {
+
+  templatePicture.dataset.pictureId = id;
+  image.src = url;
+  image.alt = description;
+  pictureComments.textContent = comments.length;
+  pictureLikes.textContent = likes;
+
+  const thumbnail = templatePicture.cloneNode(true);
+
+  picturesFragment.append(thumbnail);
+});
+
+containerPictures.append(picturesFragment);
+
+export {containerPictures};
