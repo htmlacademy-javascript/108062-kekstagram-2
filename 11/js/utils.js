d70e5b1dfcc3c55add4baa29b26a4777a46827e8
Keks <keks@htmlacademy.ru>
2024-12-14 19:06:55 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/utils.js
@@ -0,0 +1,41 @@
+const REMOVE_MESSAGE_TIMEOUT = 5000;
+const DEBOUNCE_DELAY = 500;
+
+const errorLoadDataTemplate = document.querySelector('#data-error').content;
+const body = document.querySelector('body');
+
+const showErrorMessage = (message) => {
+  const errorArea = errorLoadDataTemplate.cloneNode(true);
+
+  if (message) {
+    errorArea.querySelector('.data-error__title').textContent = message;
+  }
+
+  body.append(errorArea);
+
+  const errorLoadDataArea = body.querySelector('.data-error');
+
+  setTimeout(() => {
+    errorLoadDataArea.remove();
+  }, REMOVE_MESSAGE_TIMEOUT);
+};
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+function debounce (callback, timeoutDelay = DEBOUNCE_DELAY) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {getRandomInteger, isEscapeKey, showErrorMessage, debounce};
